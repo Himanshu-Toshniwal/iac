247c9acc331cba9c0550090ee9f6f2e3b5d41a5d
Himanshu Toshniwal <164766157+Himanshu-Toshniwal@users.noreply.github.com>
2025-07-27 11:48:13 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -56,7 +56,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "EC2 keypair"
   type        = string
-  default     = "gitopskey"
+  default     = "master three-tier-application"
 }
 
 variable "instance_name" {
